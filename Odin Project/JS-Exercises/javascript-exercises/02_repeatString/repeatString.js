f99ae2186afab27d55b58b914b20fb72d2b08cc4
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-02-13 00:40:31 +0530

    Exercise2 repeatString done

--- Odin Project/JS-Exercises/javascript-exercises/02_repeatString/repeatString.js
+++ Odin Project/JS-Exercises/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    if(num === 0){
+        return '';
+    }
+    if(num === 1){
+        return str;
+    }
+    new_str = '';
+    for(let i=0;i<num;i++){
+        new_str = new_str + str;
+    }
+    return new_str;
 };
 
 // Do not edit below this line
