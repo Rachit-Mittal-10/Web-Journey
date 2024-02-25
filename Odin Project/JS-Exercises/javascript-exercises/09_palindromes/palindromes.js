eeb17769484df47194c33629f33d1d69ebfde9bd
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-02-25 20:32:23 +0530

    09_palindrome exercise done

--- Odin Project/JS-Exercises/javascript-exercises/09_palindromes/palindromes.js
+++ Odin Project/JS-Exercises/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let str = string.toLowerCase();
+    let i = 0;
+    let j = str.length-1;
+    while(i<=j){
+        let cond_i = str[i] >= 'a' && str[i] <= 'z';
+        let cond_j = str[j] >= 'a' && str[j] <= 'z';
+        let num_cond_i = str[i] >= '0' && str[i] <= '9';
+        let num_cond_j = str[j] >= '0' && str[j] <= '9';
+        if(!cond_i && !num_cond_i){
+            i++;
+            continue;
+        }
+        if(!cond_j && !num_cond_j){
+            j--;
+        continue;
+        }
+        if(str[i] != str[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
