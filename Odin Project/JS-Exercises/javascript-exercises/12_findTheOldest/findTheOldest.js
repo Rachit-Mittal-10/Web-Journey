62345ed4625ed240e7b7842d61c3956ba26316a2
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-02-25 23:55:06 +0530

    12_findTheOldest done

--- Odin Project/JS-Exercises/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ Odin Project/JS-Exercises/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+let currentYear = new Date().getFullYear();
 
+const getAge = function(birth,death){
+    if(!death){
+        death = currentYear;
+    }
+    return death - birth;
+}
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest,currentPerson) =>{
+        let oldestAge = getAge(
+            oldest.yearOfBirth,
+            oldest.yearOfDeath
+        );
+        let currentPersonAge = getAge(
+            currentPerson.yearOfBirth,
+            currentPerson.yearOfDeath
+        );
+        return oldestAge < currentPersonAge ? currentPerson : oldest;
+    });
 };
 
 // Do not edit below this line
