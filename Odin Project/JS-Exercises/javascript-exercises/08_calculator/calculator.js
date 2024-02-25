5befad9edb650a309806618e56ad541512bf6fe9
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-02-25 19:26:08 +0530

    08_calculator exercise done

--- Odin Project/JS-Exercises/javascript-exercises/08_calculator/calculator.js
+++ Odin Project/JS-Exercises/javascript-exercises/08_calculator/calculator.js
@@ -1,33 +1,51 @@
-const add = function() {
-	
+const add = function (a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	let result = array.reduce(
+		(sum,current) => sum+current,
+		0);
+	return result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+	let result = array.reduce(
+	(ans,current) => ans*current,
+	1
+	);
+	return result;
 };
 
-const power = function() {
-	
+const power = function (base,exponent) {
+	let ans = 1;
+	for(let i=0;i<exponent;i++){
+		ans = ans*base;
+	}
+	return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	if(num === 0 || num === 0){
+		return 1;
+	}
+	let ans = 1;
+	for(let i=num;i>0;i--){
+		ans = ans*i;
+	}
+	return ans;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial
 };
