a4f2f739bb60e55cbc0b483bd97cecccc3664009
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-02-25 20:50:04 +0530

    10_fibonacci done

--- Odin Project/JS-Exercises/javascript-exercises/10_fibonacci/fibonacci.js
+++ Odin Project/JS-Exercises/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+let fibonacci_array = [0,1,1];
 
+const fibonacci = function(position, memo = {}) {
+    if(position < 0){
+        return "OOPS";
+    }
+    if(position == 0){
+        return 0;
+    }
+    if(position == 1 || position == 2){
+        return 1;
+    }
+
+    if(memo[+position]){
+        return memo[+position];
+    }
+    let ans = fibonacci(position-1) + fibonacci(position-2);
+    memo[+position] = ans;
+    return ans;
 };
 
 // Do not edit below this line
